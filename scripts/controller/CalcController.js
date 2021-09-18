d81f7c6f26711b6deddf955b6148692ecc3e5296
Alexandre Gabriel <alexandregabrielc.silva@gmail.com>
2021-09-18 16:12:00 -0300

    Aula 20 concluída

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -37,6 +37,10 @@ class CalcController {
     clearAll() {
 
         this._operation = [];
+        this._lastNumber = '';
+        this._lastOperator = ''; 
+
+
         this.setLastNumberToDisplay();
 
     }
@@ -45,6 +49,7 @@ class CalcController {
     clearEntry() {
         //O método Array.pop() remove o último elemento do array. No caso da calculadora, o botão Clear Entry apaga a entrada atual do usuario.
         this._operation.pop();
+
         this.setLastNumberToDisplay();
 
     }
@@ -171,9 +176,6 @@ class CalcController {
             if (this.isOperator(value)){
                 this.setLastOperation(value);
                 //this._operation.push(value);
-            }else if (isNaN(value)){ 
-
-                console.log('Outra coisa ', value);
             }else{
                 this.pushOperation(value);
                 this.setLastNumberToDisplay();
@@ -187,7 +189,7 @@ class CalcController {
                 
             }else{
                 let newValue = this.getLastOperation().toString() + value.toString();
-                this.setLastOperation(parseInt(newValue)); 
+                this.setLastOperation(parseFloat(newValue)); 
 
                 this.setLastNumberToDisplay();
             }            
@@ -197,6 +199,19 @@ class CalcController {
 
     setError(){
         this.displayCalc = "Error";
+    }
+
+    addDot(){
+
+        let lastOperation = this.getLastOperation();
+
+        if (this.isOperator(lastOperation) || !lastOperation){
+            this.pushOperation('0.');
+        } else {
+            this.setLastOperation(lastOperation.toString() + '.');
+        }
+
+        this.setLastNumberToDisplay();
     }
 
     execBtn(value) {
@@ -240,7 +255,7 @@ class CalcController {
                 break;
 
             case 'ponto':
-                this.addOperation('.')
+                this.addDot();
                 break; 
             
             case '0':
